faf59a03704ca2cad1cea2be8886eb0fc2e359f9
benjamin5250 <146529364+benjamin5250@users.noreply.github.com>
2023-10-04 11:15:17 +0630

    Update config.py

--- config.py
+++ config.py
@@ -17,11 +17,11 @@ LOGGER_ID = int(getenv("LOGGER_ID", "-1001554007729"))
 MONGO_DB_URI = getenv("MONGO_DB_URI", "3@cluster0.4wbux.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "1962673406").split()))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c5952790fa8235f499749.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/c5952790fa8235f499749.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/5e3c7aef33003ba82d358.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/5e3c7aef33003ba82d358.jpg")
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Lobe_JU")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/Oye_golgappu")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/purpleplaneteer")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/purpleplaneteers")
 
-STRING_SESSION = getenv("STRING_SESSION", "lMvbNkCZIXhncQS_EtlfV2Rn4q7pGrS9-FXi1AC1L7O1p4LZ31owYnpfFJNE7hr5UNWAH5LYmFc2EeG1tiz8eP2t44U2ztMA82S00eYF2zlOeHqbuov-q2uVWdvBW5V96PQHH1hE1mXzCJc2Q-xIOrOTpGrCwQbHm8tv-oaiNvK__miFB8jjByAAAAAHTel-MA")
+STRING_SESSION = getenv("STRING_SESSION", "BQFEoW8AbwHdgJtvoPv4h42F0OWb2YAmHdMB0bdi4mxDYxM-HTAcdZooOoncccwRfmPZvR0dGZI3H9ePGfJvIzlTf2ioYhI1e0vwxJVfmvI0ylx8kuBJVJj1-sqXWmgmzkIU1pURFFnPOlklBntbz2v_t1zgyukKGwTNTIarMd3Ny5hFCsDvshtoVsCrQ3Gk-rQHxF0TgcKx03apVcm5Qk57nuynCZz3qmYaSNQZuOlJ0BeRNRZjGP8Dg2lFI_nonxsdWDb5xCLHEnaKF95QPt4sKa9wLeEW31_PDtJ6DCo4p9WEnqeDGWeuimI-hg9cHO2SIPmcVx0yQ_4xLViXBvYCN0rlYwAAAAF6X2woAA")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1962673406").split()))
